cea7c0ac279269a67c7c6aecc4f95a8919bf3a5e
Anuj mishra <captainanuj2004@gmail.com>
2025-12-09 05:30:04 +0000

    Day-2 of js...

---
+++ Day-2-js_training/investigation_variables.js
@@ -0,0 +1,60 @@
+// Day 2 of training...
+
+// Investigation study on variables
+
+// -> Display all the values of variables.
+// -> Try to change value of each and every variable and display the error message if any.
+// -> Redeclare variable with same name and initialize with new value. and display the output.
+
+
+// "use strict" keyword is use to follow modern standard in js, it also stricts hoisting..
+
+
+
+
+
+// var a=10;
+// const b=33;
+// let c=56;
+// console.log(a);
+// console.log(b);
+// //making scope using {}
+// {
+//     var gh=89;
+//     console.log(gh);
+//     let gg=99;
+//     console.log(gg);
+// }
+// // console.log(gg); error because let limits its scope to block
+// console.log(gh); //var does not limits its scope hence no error
+// // b=45;//error assignment to constant variable...
+// console.log(c);
+
+
+// let a="abc";
+// let b="";
+// let c=null;
+// let d;
+// console.log(a); //abc prints
+// console.log(b); //empty value
+// console.log(c); // null
+// console.log(d); //undefined
+
+// let a="abc";
+// let b ="";
+// let d;
+// console.log(typeof(null)); //gives object ?? why - bug in js
+
+let score="33aab";
+console.log(typeof(score));
+console.log(typeof score);
+ score=Number(score)
+console.log(typeof score);
+console.log(score);
+
+
+//Remaining topics - Date , timestamp....
+
+
+
+
