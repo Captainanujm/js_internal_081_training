35c9e20ca18c3eaa9deb15ead49e961a2d6405a4
Anuj mishra <captainanuj2004@gmail.com>
2025-12-10 02:29:28 +0000

    program to check voting eligibility

---
+++ Day-2-js_assignment/basic/voting.js
@@ -0,0 +1,10 @@
+function canVote(age) {
+    if (age >= 18) {
+        return "Eligible to vote";
+    } else {
+        return "Not eligible to vote";
+    }
+}
+
+console.log(canVote(20)); // Eligible to vote
+console.log(canVote(16)); // Not eligible to vote
