e9d9ad9ed474e9d79900ccffb177e4b777cdabfc
Anuj mishra <captainanuj2004@gmail.com>
2025-12-10 02:09:02 +0000

    maximum among three numbers in js

---
+++ Day-2-js_assignment/basic/max_among_3.js
@@ -0,0 +1,13 @@
+let a=35;
+let b=78;
+let c=1;
+if(a>b&&a>c){
+    console.log("a is maximum");
+}
+else if(b>a&&b>c){
+    console.log("b is maximum");
+}else if(c>a&&c>b){
+    console.log("c is maximum");
+}else{
+    
+}
