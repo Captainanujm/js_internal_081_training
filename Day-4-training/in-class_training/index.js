2ffbdf76ec5175c2b8004586102f01d786ee0403
Anuj mishra <captainanuj2004@gmail.com>
2025-12-11 05:31:01 +0000

    operations on html and date

---
+++ Day-4-training/in-class_training/index.js
@@ -0,0 +1,10 @@
+let myDate=new Date();
+console.log(myDate.toString());
+
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString());
+console.log(myDate.toJSON());
+console.log(myDate.getFullYear());
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+
