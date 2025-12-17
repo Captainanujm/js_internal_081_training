68b36d79c1de1e07fb38ebd8492775ccc863f63e
Anuj mishra <captainanuj2004@gmail.com>
2025-12-17 05:21:08 +0000

    enhanced css

--- Day-8-final_evaluation/index.js
+++ Day-8-final_evaluation/index.js
@@ -1,4 +1,4 @@
-const params=newURLSearchParams(window.location.search);
+const params=new URLSearchParams(window.location.search);
 const value1=params.get("input-name");
 const value2=params.get("input-email");
 document.querySelector("#para").innerText=`your name ${value1}`;
